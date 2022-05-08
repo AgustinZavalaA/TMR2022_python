a2204d116e9a604e658b2bc604ca237c40ce2794
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-05-08 16:57:21 -0500

    capture crea carpeta y formateo

--- capture_test.py
+++ capture_test.py
@@ -1,32 +1,44 @@
 import cv2
+import os
 
-cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
-cv2.namedWindow("test")
+def main():
+    pathExists = os.path.exists("fotos/")
 
-img_counter = 0
+    if not pathExists:
+        os.mkdir("fotos/")
 
-while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    cv2.imshow("test", frame)
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
-    k = cv2.waitKey(1)
-    if k%256 == 27: 
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "fotos/opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
+    cv2.namedWindow("test")
 
-cam.release()
+    img_counter = 0
 
-cv2.destroyAllWindows()
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("failed to grab frame")
+            break
+        cv2.imshow("test", frame)
+
+        k = cv2.waitKey(1)
+        if k % 256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...")
+            break
+        elif k % 256 == 32:
+            # SPACE pressed
+            img_name = "fotos/opencv_frame_{}.png".format(img_counter)
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            img_counter += 1
+
+    cam.release()
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
