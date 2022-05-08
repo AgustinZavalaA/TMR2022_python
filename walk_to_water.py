401bdebbc718e831a476add149fef3dc5e1a8d20
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-05-08 12:24:51 -0500

    quite el muestreo de imagen y habilitar ctrl c

--- walk_to_water.py
+++ walk_to_water.py
@@ -14,35 +14,39 @@ def main(hsv_min: tuple[int, int, int], hsv_max: tuple[int, int, int]):
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print("Error al leer la camara")
-            break
+    try:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                print("Error al leer la camara")
+                break
 
-        water_roi = frame[300:360, :]
-        water_roi = cv2.cvtColor(water_roi, cv2.COLOR_BGR2HSV)
-        masked_water = cv2.inRange(water_roi, hsv_min, hsv_max)
+            water_roi = frame[300:360, :]
+            water_roi = cv2.cvtColor(water_roi, cv2.COLOR_BGR2HSV)
+            masked_water = cv2.inRange(water_roi, hsv_min, hsv_max)
 
-        # print if water is detected, majority of the pixels are 1
-        threshold = 0.8
-        if (
-            np.sum(masked_water)
-            > masked_water.shape[0] * masked_water.shape[1] * 255 * threshold
-        ):
-            motors.stop()
-            print("Water detected")
-        else:
-            motors.move(True, 15, True)
-            motors.move(False, 15, True)
-            print("No water detected")
+            # print if water is detected, majority of the pixels are 1
+            threshold = 0.8
+            if (
+                np.sum(masked_water)
+                > masked_water.shape[0] * masked_water.shape[1] * 255 * threshold
+            ):
+                motors.stop()
+                print("Water detected")
+            else:
+                motors.move(True, 15, True)
+                motors.move(False, 15, True)
+                print("No water detected")
 
-        cv2.imshow("frame", frame)
-        cv2.imshow("masked_water", masked_water)
+            # cv2.imshow("frame", frame)
+            # cv2.imshow("masked_water", masked_water)
 
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            print("Exiting out")
-            break
+            # if cv2.waitKey(1) & 0xFF == ord("q"):
+            #     print("Exiting out")
+            #     break
+    except KeyboardInterrupt:
+        motors.stop()
+        print("Ctrl+C pressed. Exiting...")
 
 
 if __name__ == "__main__":
