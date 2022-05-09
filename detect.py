a7baea866aa5fc5b091a583b7d268045e6041a8e
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-05-08 20:47:16 -0500

    rango extendido a 20% para detenerse

--- detect.py
+++ detect.py
@@ -109,8 +109,8 @@ def run(
                     abs(distance_from_center), 0, image.shape[1] // 2, 0, 65
                 )
                 print(vel)
-                # si el objeto esta en la mitad de la imagen (dentro del 10%), no hace nada
-                if abs(distance_from_center) < image.shape[1] // 2 * 0.1:
+                # si el objeto esta en la mitad de la imagen (dentro del 20%), no hace nada
+                if abs(distance_from_center) < image.shape[1] // 2 * 0.2:
                     print("stopped")
                     motors.stop()
                 # si el objeto esta a la derecha, se mueve a la izquierda
