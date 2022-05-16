aec2196246ba70c49898373f0d774aecaa098a9e
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-05-15 23:11:42 -0500

    probando bocina

--- modules/ArduinoSerialComm.py
+++ modules/ArduinoSerialComm.py
@@ -50,6 +50,8 @@ def main() -> None:
     time.sleep(3)
     print(arduino.communicate("3"))
     time.sleep(2)
+    arduino.communicate("6")
+    time.sleep(2)
 
     # si se necesita hacer un communicate("2") se necesita implementar un sleep seguido de este
     try:
