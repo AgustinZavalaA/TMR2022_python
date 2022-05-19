419875ca14c8a2eb3a7107d578b9459c15533b5b
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-05-19 00:27:51 -0500

    creacion de esqueleto de pruebas de conceptos

---
+++ pruebas_concepto/main.py
@@ -0,0 +1,32 @@
+import time
+
+from ..modules.ArduinoSerialComm import ArduinoComm
+
+
+def main():
+    arduino = ArduinoComm(port="/dev/ttyACM0", baudrate=115200, timeout=0.1)
+    while True:
+        change, mode, _ = arduino.communicate(data="1")
+        if change == 1:
+            if mode == 0:
+                continue
+            if mode == 1:
+                print("Prueba de desplazamiendo en lado menor")
+            if mode == 2:
+                print("Prueba de evasion de mar")
+            if mode == 3:
+                print("Prueba de evasion de objetos")
+            if mode == 4:
+                print("Prueba de localizacion de residuo")
+            if mode == 5:
+                print("Prueba de recoleccion de residuo")
+            if mode == 6:
+                print("Prueba de deposito de residuo")
+            if mode == 7:
+                print("k pendejo papi")
+                break
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
